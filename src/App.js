a886617dc22d499319dcc08e135f41b19d930449
Daniel <dannywhiting17@yahoo.com>
2023-01-24 21:42:55 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
+
+  const apiKey = '3d32270e57e663560cc91ba80b82ab00'
+
+  useEffect(() => {
+
+    console.log('aksldjfakldjflak')
+
+    
+    axios({
+      method: "GET",
+      url:`https://api.openweathermap.org/data/2.5/weather?lat=42.73&lon=84.55&appid=${apiKey}`,}) 
+    .then((response) => {
+      console.log(response.data);
+      setTemp(response.data.main.temp.fahrenheit)
+     
+    })
+    .catch((err) => console.log(err));
+    
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {
+
+            <h1>{temp}</h1>
+      
+      }
     </div>
   );
 }
